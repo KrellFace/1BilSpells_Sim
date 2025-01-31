2987f4387f56bcb7966b6a0de6671c12e68758d7
KrellFace <owithington@hotmail.co.uk>
2025-01-31 17:11:11 +0000

    Remove Test File

--- func/figureGen.py
+++ func/figureGen.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.rcParams.update({'font.size': 14}) 
 
 def gen_heatmap(x_vals, y_vals, bucket_count, x_label, y_label, title, out_path):
     # Define bin edges
@@ -21,6 +24,15 @@ def gen_heatmap(x_vals, y_vals, bucket_count, x_label, y_label, title, out_path)
     plt.savefig(out_path+".png")
     #plt.show()
 
+def gen_histogram(values, bin_count, x_label, y_label, title, out_path):
+    plt.hist(values, bins=bin_count, edgecolor='black')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.savefig(out_path+".png")
+    plt.show()
+
+
 
 def gen_all_figures(spell_properties, max_nodes_per_spell: int, sim_length: int, outpath):
     
@@ -32,3 +44,26 @@ def gen_all_figures(spell_properties, max_nodes_per_spell: int, sim_length: int,
             x_vals.append(sp.linearity)
             y_vals.append(sp.oneTDps/sim_length)          
     gen_heatmap(x_vals, y_vals, 20, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps")
+
+    gen_histogram(y_vals, 20, "1DPS", "Frequency", "Distribution of Spell Damage",outpath+"dpshisto")
+    
+def gen_all_figures_from_csv(max_nodes_per_spell: int, sim_length: int, in_csv, outpath):
+
+    df = pd.read_csv(in_csv)
+
+    all_data = df.to_numpy()
+
+    #print(all_data)
+    
+    #Generate linearity vs 1dps image
+    x_vals = []
+    y_vals = []
+    for val in all_data:
+        #print(val)
+        if (val[2] == max_nodes_per_spell):
+            x_vals.append(val[3])
+            y_vals.append(val[4]/sim_length)          
+    gen_heatmap(x_vals, y_vals, 10, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps")
+
+    gen_histogram(y_vals, 20, "1DPS", "Frequency", "Distribution of Spell Damage",outpath+"dpshisto")
+
--- main_SpellSim.py
+++ main_SpellSim.py
@@ -10,18 +10,20 @@ NODES_PER_NODETYPE = 3
 ATTEMPTS_PER_SPELL = 20
 TOTAL_SPELLS = 100000
 SPELL_SIM_LENGTH = 30
-RUN_NAME = "100Thousand_FullRun"
+RUN_NAME = "FiguresOnly"
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 #Define the file path for the CSV
-csv_file_path = os.path.join(current_directory, "output", RUN_NAME)
+files_outpath = os.path.join(current_directory, "output", RUN_NAME)
 
-os.makedirs(os.path.dirname(csv_file_path), exist_ok=True)
+os.makedirs(os.path.dirname(files_outpath), exist_ok=True)
 
-spell_properties = full_spell_generation_run(csv_file_path, TOTAL_SPELLS, enum_SpellQualityHeuristic.DPS, ATTEMPTS_PER_SPELL, NODES_PER_NODETYPE, SPELL_SIM_LENGTH)
+#spell_properties = full_spell_generation_run(csv_file_path, TOTAL_SPELLS, enum_SpellQualityHeuristic.DPS, ATTEMPTS_PER_SPELL, NODES_PER_NODETYPE, SPELL_SIM_LENGTH)
 
 
-gen_all_figures(spell_properties, NODES_PER_NODETYPE*3,SPELL_SIM_LENGTH,csv_file_path)
+#gen_all_figures(spell_properties, NODES_PER_NODETYPE*3,SPELL_SIM_LENGTH,csv_file_path)
+
+gen_all_figures_from_csv(NODES_PER_NODETYPE*3,SPELL_SIM_LENGTH,"output/100Thousand_FullRun_data.csv", files_outpath)
 
 #x = np.random.randn(100000) 
 #y = np.random.randn(100000) 
