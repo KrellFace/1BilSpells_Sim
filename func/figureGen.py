f744607d1c3a9db513eb77c4e4336cc2f5632975
KrellFace <owithington@hotmail.co.uk>
2025-03-27 14:15:41 +0000

    Hexmap Generation

--- func/figureGen.py
+++ func/figureGen.py
@@ -6,22 +6,53 @@ plt.rcParams.update({'font.size': 14})
 
 def gen_heatmap(x_vals, y_vals, bucket_count, x_label, y_label, title, out_path):
     # Define bin edges
-    x_bins = np.linspace(0, max(x_vals), bucket_count)  
-    y_bins = np.linspace(0, max(y_vals), bucket_count) 
+    x_bins = np.linspace(0, 1, bucket_count+1)  
+    y_bins = np.linspace(0, max(y_vals), bucket_count+1) 
+
+    #print(x_bins)
 
     # Compute 2D histogram
-    heatmap, xedges, yedges = np.histogram2d(x_vals, y_vals, bins=[x_bins, y_bins])
+    heatmap, xedges, yedges = np.histogram2d(x_vals, y_vals, bins=[x_bins, y_bins])#, range = [[0,1],[0,max(y_vals)]])
 
     # Plot heatmap
     plt.imshow(heatmap.T, origin='lower', aspect='auto',
-            extent=[x_bins[0], x_bins[-1], y_bins[0], y_bins[-1]],
-            cmap='hot')
+            extent=[0, 1, 0, max(y_vals)],
+            cmap='gist_heat' )
 
     plt.colorbar(label='Count')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.xlim(0, 1) 
+    plt.ylim(0, max(y_vals))
     plt.title(title)
     plt.savefig(out_path+".png")
+    plt.close()
+    #plt.show()
+
+def gen_hexheatmap(x_vals, y_vals, bucket_count, x_label, y_label, title, out_path):
+    # Define bin edges
+    x_bins = np.linspace(0, max(x_vals), bucket_count+1)  
+    y_bins = np.linspace(0, max(y_vals), bucket_count+1) 
+
+    # Compute 2D histogram
+    #heatmap, xedges, yedges = np.histogram2d(x_vals, y_vals, bins=[x_bins, y_bins])
+
+    # Plot heatmap
+    #plt.imshow(heatmap.T, origin='lower', aspect='auto',
+    #        extent=[x_bins[0], x_bins[-1], y_bins[0], y_bins[-1]],
+    #        cmap='hot')
+    
+    plt.hexbin(x_vals, y_vals, gridsize = 10,  
+               bins = None, cmap ='gist_heat', extent= [0, 1, 0, max(y_vals)]) 
+
+    plt.colorbar(label='Count')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.xlim(0, 1) 
+    plt.ylim(0, max(y_vals))
+    plt.title(title)
+    plt.savefig(out_path+".png")
+    plt.close()
     #plt.show()
 
 def gen_histogram(values, bin_count, x_label, y_label, title, out_path):
@@ -30,20 +61,23 @@ def gen_histogram(values, bin_count, x_label, y_label, title, out_path):
     plt.ylabel(y_label)
     plt.title(title)
     plt.savefig(out_path+".png")
-    plt.show()
+    plt.close()
+    #plt.show()
 
 
 
-def gen_all_figures(spell_properties, max_nodes_per_spell: int, sim_length: int, outpath):
+def gen_all_figures(spell_properties, min_nodes_per_spell: int, sim_length: int, outpath):
     
     #Generate linearity vs 1dps image
     x_vals = []
     y_vals = []
     for sp in spell_properties:
-        if (sp.uniqueNodeCount == max_nodes_per_spell):
+        if (sp.uniqueNodeCount >= min_nodes_per_spell):
             x_vals.append(sp.linearity)
             y_vals.append(sp.oneTDps/sim_length)          
-    gen_heatmap(x_vals, y_vals, 20, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps")
+    gen_heatmap(x_vals, y_vals, 10, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps")
+
+    gen_hexheatmap(x_vals, y_vals, 10, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps-Hex")
 
     gen_histogram(y_vals, 20, "1DPS", "Frequency", "Distribution of Spell Damage",outpath+"dpshisto")
     
@@ -66,4 +100,6 @@ def gen_all_figures_from_csv(max_nodes_per_spell: int, sim_length: int, in_csv, 
     gen_heatmap(x_vals, y_vals, 10, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps")
 
     gen_histogram(y_vals, 20, "1DPS", "Frequency", "Distribution of Spell Damage",outpath+"dpshisto")
+
+    gen_hexheatmap(x_vals, y_vals, 10, "Linearity", "1DPS", "Spell Linearity vs DPS", outpath+"linvsdps-Hex")
 
--- main_SpellSim.py
+++ main_SpellSim.py
@@ -6,11 +6,11 @@ from nodeTypes import *
 
 #FULL SPELL GENERATION RUNS
 
-NODES_PER_NODETYPE = 3
+NODES_PER_NODETYPE = 5
 ATTEMPTS_PER_SPELL = 20
-TOTAL_SPELLS = 100000
+TOTAL_SPELLS = 1000
 SPELL_SIM_LENGTH = 30
-RUN_NAME = "FiguresOnly"
+RUN_NAME = "NEW_HEATMAP"
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 #Define the file path for the CSV
@@ -18,14 +18,9 @@ files_outpath = os.path.join(current_directory, "output", RUN_NAME)
 
 os.makedirs(os.path.dirname(files_outpath), exist_ok=True)
 
-#spell_properties = full_spell_generation_run(csv_file_path, TOTAL_SPELLS, enum_SpellQualityHeuristic.DPS, ATTEMPTS_PER_SPELL, NODES_PER_NODETYPE, SPELL_SIM_LENGTH)
+#spell_properties = full_spell_generation_run(files_outpath, TOTAL_SPELLS, enum_SpellQualityHeuristic.DPS, ATTEMPTS_PER_SPELL, NODES_PER_NODETYPE, SPELL_SIM_LENGTH)
 
 
-#gen_all_figures(spell_properties, NODES_PER_NODETYPE*3,SPELL_SIM_LENGTH,csv_file_path)
+#gen_all_figures(spell_properties, 8,SPELL_SIM_LENGTH,files_outpath)
 
-gen_all_figures_from_csv(NODES_PER_NODETYPE*3,SPELL_SIM_LENGTH,"output/100Thousand_FullRun_data.csv", files_outpath)
-
-#x = np.random.randn(100000) 
-#y = np.random.randn(100000) 
-
-#gen_heatmap(x, y,20, "xlabel", "ylabel", "funny title", csv_file_path+"_lol")
+gen_all_figures_from_csv(9,SPELL_SIM_LENGTH,"output/1HundredThousandSpells-30 SecondSim_data.csv", files_outpath)
